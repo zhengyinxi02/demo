05a1e6328ab8e4e9db891a4a1f9c459d7a1d3286
zhengyinxi <zyx@zyx.com>
2019-09-08 15:51:53 +0800

    add exit/quit command

---
+++ src/main/java/com/example/demo/shellcommands/ExitShellCommand.java
@@ -0,0 +1,13 @@
+package com.example.demo.shellcommands;
+
+import org.springframework.shell.ExitRequest;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class ExitShellCommand  {
+    @ShellMethod(value = "Exit the shell.", key = {"quit", "exit"})
+    public void quit() {
+        throw new ExitRequest();
+    }
+}
