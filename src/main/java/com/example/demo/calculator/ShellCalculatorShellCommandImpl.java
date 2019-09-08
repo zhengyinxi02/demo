f0ad2004f5c969d29bb06dc5eb72d1fdb4cf8f91
zhengyinxi <zyx@zyx.com>
2019-09-08 22:45:57 +0800

    find error index in rawtext

--- src/main/java/com/example/demo/calculator/ShellCalculatorShellCommandImpl.java
+++ src/main/java/com/example/demo/calculator/ShellCalculatorShellCommandImpl.java
@@ -6,10 +6,10 @@ import com.example.demo.calculator.exception.InsufficientParameterException;
 import com.example.demo.event.Event;
 import com.example.demo.event.EventDispatcher;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.Input;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.util.EmptyStackException;
 import java.util.List;
@@ -27,27 +27,30 @@ public class ShellCalculatorShellCommandImpl extends AbstractShellCalculatorComm
 
     @Override
     public Object run(Input input) {
-        List<String> words = input.words().stream().filter(w-> !StringUtils.isEmpty(w)).collect(Collectors.toList());
+        //use counter for position index
+        String rawText = input.rawText();
+        List<String> words = input.words().stream().filter(w -> !StringUtils.isEmpty(w)).collect(Collectors.toList());
         int index = 0;
-        for(String word: words){
-            index++;
+        for (String word : words) {
+            index = StringUtils.indexOf(rawText, word, index);
+
             Event event = null;
             try {
                 event = factory.create(word);
             } catch (UnsupportedOperationException e) {
-                return "operator " + word+ " (position: "+ index +"): " + "invalid command\n" + stackToString();
+                return "operator " + word + " (position: " + (index + 1) + "): " + "invalid command\n" + stackToString();
             }
             try {
                 handler.dispatch(event);
             } catch (EmptyStackException e) {
-                if(event instanceof Operator){
+                if (event instanceof Operator) {
                     Operator o = (Operator) event;
-                    return "operator " + o.getOperator()+ " (position: "+ index +"): " + "insufficient parameters\n" + stackToString();
+                    return "operator " + o.getOperator() + " (position: " + (index + 1) + "): " + "insufficient parameters\n" + stackToString();
                 }
             } catch (InsufficientParameterException e) {
-                return "operator " + e.getOperator()+ " (position: "+ index +"): " + "insufficient parameters\n" + stackToString();
+                return "operator " + e.getOperator() + " (position: " + (index + 1) + "): " + "insufficient parameters\n" + stackToString();
             } catch (IllegalOperatorException e) {
-                return "operator " + e.getOperator()+ " (position: "+ index +"): " + "illegal parameters\n" + stackToString();
+                return "operator " + e.getOperator() + " (position: " + (index + 1) + "): " + "illegal parameters\n" + stackToString();
             }
         }
         return stackToString();
